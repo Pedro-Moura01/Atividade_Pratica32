ef43ddd9eba7933940ffb6257bd881a1829b819b
Pedro-Moura01 <pedromourapims9@gmail.com>
2024-10-29 10:21:20 -0300

    Adicione imagem de pizza ao index.php

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Document</title>
+    <title>Atividade Prática 32</title>
 </head>
 <body>
-    <h1>Pedro Ian e Luan Henrique</h1>
+    <h1>Pedro Ian e Luan Henrique<h1>
+        <a href="index.php" class="imagens">
+        <img src="pizza.JPEG" alt="" />
 </body>
 </html>
